319a94f479f2dc87f1a22f8d37b3353f0d236ff0
Desmond Phee <nocolour@gmail.com>
2021-02-04 17:15:14 +0800

    Initial commit

--- 17-Flask–Mail.py
+++ 17-Flask–Mail.py
@@ -4,10 +4,10 @@ from flask_mail import Mail, Message  # Flask-Mail
 app = Flask(__name__)
 mail = Mail(app)
 
-app.config['MAIL_SERVER'] = 'mail.hwathai.my'
+app.config['MAIL_SERVER'] = 'mail.abc.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'desmond.phee@hwathai.my'
-app.config['MAIL_PASSWORD'] = 'P@SS4392'
+app.config['MAIL_USERNAME'] = 'desmond@abc.com'
+app.config['MAIL_PASSWORD'] = 'password'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
@@ -15,7 +15,7 @@ mail = Mail(app)
 
 @app.route("/")
 def index():
-    msg = Message('Hello', sender='desmond.phee@hwathai.my', recipients=['nocolour@gmail.com'])
+    msg = Message('Hello', sender='desmond@abc.com', recipients=['xyz@gmail.com'])
     msg.body = "Hello Flask message sent from Flask-Mail"
     mail.send(msg)
     return "Sent"
